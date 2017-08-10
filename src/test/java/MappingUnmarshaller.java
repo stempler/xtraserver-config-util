62a4aadd334de51806687ac783d778c7c1ed33e2
azahnen <zahnen@interactive-instruments.de>
2017-08-10 11:19:16 +0200

    setup unit tests

--- src/test/java/MappingUnmarshaller.java
+++ src/test/java/MappingUnmarshaller.java
@@ -1,9 +1,11 @@
-package de.interactive_instruments.xtraserver.util;
-
 import javax.xml.bind.JAXBException;
 
 import de.interactive_instruments.xtraserver.config.api.FeatureTypeMapping;
 import de.interactive_instruments.xtraserver.config.api.XtraServerMapping;
+import de.interactive_instruments.xtraserver.util.ApplicationSchema;
+import de.interactive_instruments.xtraserver.util.MappingParser;
+import de.interactive_instruments.xtraserver.util.Namespaces;
+import org.junit.Test;
 import org.xml.sax.SAXException;
 import de.interactive_instruments.xtraserver.schema.*;
 
@@ -12,13 +14,15 @@ import java.util.Collection;
 
 public class MappingUnmarshaller
 {
-   public static void main( String[] args ) throws JAXBException, SAXException, FileNotFoundException {
-      if( args.length < 2 ) {
-         System.out.println( "\nBitte XSD-Schema und XML-Dokument angeben" );
-         return;
-      }
-      FeatureTypes featureTypes = MappingParser.unmarshal( args[0], args[1], FeatureTypes.class );
-      XtraServerMapping xsm = new XtraServerMapping(featureTypes, new ApplicationSchema(args[2], new Namespaces()));
+   private final static String MAPPING_SCHEMA = "src/main/resources/XtraServer_Mapping.xsd";
+   private final static String MAPPING = "src/main/resources/XtraSrvConfig_Mapping.inc.xml";
+   private final static String APPLICATION_SCHEMA = "src/main/resources/Cities.xsd";
+
+   @Test
+   public void unmarshal() throws JAXBException, SAXException, FileNotFoundException {
+
+      FeatureTypes featureTypes = MappingParser.unmarshal( MAPPING_SCHEMA, MAPPING, FeatureTypes.class );
+      XtraServerMapping xsm = new XtraServerMapping(featureTypes, new ApplicationSchema(APPLICATION_SCHEMA, new Namespaces()));
       xsm.print();
    }
 }
